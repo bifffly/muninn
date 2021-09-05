84b1f64e009350f223c5241dca59f012a020ba4b
Bryce Colson <brycec17@vt.edu>
2021-09-05 11:21:33 -0700

    Added request-handling functionality

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,34 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        let req = handle_request(&stream);
+        let path = request_to_path(req);
+        send_response(stream, path);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+fn handle_request(mut stream: &TcpStream) -> String {
+    let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+    let mut v = vec![];
+    for byte in buffer {
+        match byte {
+            0 => break,
+            _ => v.push(byte),
+        }
+    }
+    return String::from_utf8_lossy(&v[..]).to_string();
+}
 
-    let content = fs::read_to_string("example/home.ryd").unwrap();
+fn request_to_path(req: String) -> String {
+    let home = "example".to_string();
+    return home + &req;
+}
 
-    let response = format!("rydja0.1\t10\r\n{}", content);
+fn send_response(mut stream: TcpStream, path: String) {
+    let response;
+    let content = fs::read_to_string(path).unwrap();
+    response = format!("rydja1\tA\r\n{}", content);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
